b8860dcd4ad60f369a9ab20d9614f0828d2fc389
Andy Barreiro <andybarreiro23cb@gmail.com>
2026-02-11 18:22:20 -0300

    deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
+  base: '/learnsoft/',
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
